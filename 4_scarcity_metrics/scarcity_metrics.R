4af3d43e682c5b16cc10618ba341dbfd9a33d401
thora-9 <tejasvi_9@hotmail.com>
2024-08-16 22:41:27 -0400

    updating scarcity scripts; changing to function based

--- 4_scarcity_metrics/scarcity_metrics.R
+++ 4_scarcity_metrics/scarcity_metrics.R
@@ -23,7 +23,8 @@ library(haven)
 library(pbapply)
 
 
-source('myfunctions.R')
+source('0_environment/myfunctions.R')
+source('4_scarcity_metrics/workflow_functions.R')
 
 #paths
 proj_dir = "~/Dropbox/WB/livable_planet/"
@@ -42,8 +43,8 @@ fishnet.r =
 ####Load World Regions
 wb_regions = 
   st_read(paste0(database, "Admin/WB_Regions/WB_countries_Admin0_10m_QGIS_fixed.gpkg")) %>%
-  filter(TYPE != 'Dependency') %>%
-  dplyr::select(WB_NAME, ISO_A2, ISO_A3, ISO_N3, TYPE, REGION_WB) %>%
+  #filter(TYPE != 'Dependency') %>%
+  dplyr::select(WB_NAME, WB_A3, ISO_A3, ISO_N3, TYPE, REGION_WB) %>%
   mutate(rownumber = row_number()) %>% #Need to create custom ID; otherwise some duplicates due to random colonial territories
   mutate(ISO_N3 = as.numeric(ISO_N3))
 
@@ -91,38 +92,13 @@ ds1 =
   merge(date.seq, by.x = 'time', by.y = 'netcdf_time', all.x = T) %>%
   .[, cell_id := paste0(lat, lon)]
 
-#Reference for unit conversion: https://www.researchgate.net/post/How-to-convert-cmip5-monthly-precipitation-kg-m2-s1-into-mm-month
-env_factor = 0.4 #proportion of runoff left for environmental demands
 
-#Yearly
-# ds1_transform = 
-#   ds1 %>%
-#   dplyr::select(lon:cell_id, qs, qsb, airruse, airrusegreen, arainfusegreen, pirrww, pirruse, airrww) %>%
-#   filter(year>2000) %>% #Subset to the recent 5 years
-#   mutate(across(qs:airrww, ~ .x*86400)) %>% #(kg m-2 s-1) to (kg m-2 day-1)
-#   mutate(across(qs:airrww, ~ .x*(days/1000))) %>% #(kg m-2 day-1) to (kg m-2 month-1) # kg m-2 == (1/1000) m 
-#   group_by(cell_id, lon, lat, year) %>% #Convert from monthly to yearly by taking the sum
-#   summarise(across(qs:airrww, ~ sum(.x, na.rm = T))) %>%
-#   group_by(cell_id, lon, lat) %>% #Average across yearly
-#   summarise(across(qs:airrww, ~ mean(.x, na.rm = T))) %>%
-#   mutate(bwa_m_year = (1-env_factor)*(qs + qsb),
-#          gwa_m_year = airrusegreen + arainfusegreen) %>%
-#   as.data.table()
+withdrawal_based = F
 
-#Monthly
-ds1_transform = 
-  ds1 %>%
-  dplyr::select(lon:cell_id, qs, qsb, adomuse:pirrww) %>%
-  #filter(year>2000) %>% #Subset to the recent 5 years
-  mutate(across(qs:pirrww, ~ .x*86400)) %>% #(kg m-2 s-1) to (kg m-2 day-1)
-  mutate(across(qs:pirrww, ~ .x*(days/1000))) %>% #(kg m-2 day-1) to (kg m-2 month-1) # kg m-2 == (1/1000) m 
-  # group_by(cell_id, lon, lat, year) %>% #Convert from monthly to yearly by taking the sum
-  # summarise(across(qs:airrww, ~ sum(.x, na.rm = T))) %>%
-  # group_by(cell_id, lon, lat) %>% #Average across yearly
-  # summarise(across(qs:airrww, ~ mean(.x, na.rm = T))) %>%
-  mutate(bwa_m_month = (1-env_factor)*(qs + qsb),
-         gwa_m_month = airrusegreen + arainfusegreen) %>%
-  as.data.table()
+##########################################################################################
+#Transform PCR-GLOBWB data
+ds1_transform = pre_process_PCR_GLOBWB(ds1, withdrawal_based)
+
 
 #Checks
 check_raster = rasterFromXYZ(ds1_transform[,.(lon, lat, gwa_m_month)])
@@ -144,7 +120,7 @@ ref_raster_df =
   terra::as.data.frame(xy=T, cells=FALSE) %>%
   as.data.table() %>%
   dplyr::rename(lon = x, lat = y, rownumber = layer) %>%
-  merge(wb_regions %>% st_drop_geometry() %>% dplyr::select(WB_NAME, ISO_A3, rownumber, REGION_WB),
+  merge(wb_regions %>% st_drop_geometry() %>% dplyr::select(WB_NAME, ISO_A3, WB_A3, rownumber, REGION_WB),
         by = 'rownumber', all.x = T) %>%
   drop_na()
 
@@ -187,11 +163,6 @@ ds2 =
 #Reference for unit conversion: https://www.researchgate.net/post/How-to-convert-cmip5-monthly-precipitation-kg-m2-s1-into-mm-month
 ds2_transform = 
   ds2 %>%
-  #filter(year>2000) %>% #Subset to the recent 5 years
-  # group_by(cell_id, lon, lat, year) %>% #Convert from monthly to yearly by taking the sum
-  # summarise(ETc_m3_year = sum(ETc, na.rm = T)) %>%
-  # group_by(cell_id, lon, lat) %>% #Average across yearly
-  # summarise(ETc_m3_year = mean(ETc_m3_year, na.rm = T)) %>%
   as.data.table() %>%
   filter(ETc != 0)#filter(ETc_m3_year != 0)
 
@@ -213,94 +184,77 @@ WF_data =
 # matches = WF_data %>% filter(Country_WB %in% wb_regions$WB_NAME)
 # non_matches = WF_data %>% filter(!Country_WB %in% wb_regions$WB_NAME)
 
+##########################################################################################
+#Cropland Data
+path2data = paste0(database, 'Agriculture/Cropped Area/Potapov_cropland_extent_2022/')
+
+#Load the irrigated area map by Meier et al. (2018)
+rast_crop = 
+  rast(paste0(path2data, 'Global_cropland_3km_2019.tif')) %>%
+  terra::project(fishnet.r, 'average')
+
+#Create layers for 3 thresholds -> >0, >1%, >10% 
+crop_thresholds = 
+  c(ifel(rast_crop > 5, 1, 0), 
+    ifel(rast_crop >= 10, 1, 0), 
+    ifel(rast_crop >= 20, 1, 0))
+
+#In percentages
+names(crop_thresholds) = c('cropland_5', 'cropland_10', 'cropland_20')
+
+#Convert to dataframe
+df_crop =
+  as.data.frame(crop_thresholds, xy = T) %>%
+  dplyr::rename(lon = x, lat = y) %>%
+  as.data.table()
 
 ##########################################################################################
 #Merge all
 
-# ds_all = 
-#   ds1_transform %>%
-#   merge(ref_raster_df, by = c('lon', 'lat'), all.x=T) %>%
-#   merge(ds2_transform, by = c('lon', 'lat'), all.x=T) %>%
-#   drop_na %>%
-#   dplyr::select(lon, lat, area:REGION_WB, airruse, pirruse, bwa_m_year, gwa_m_year, ETc_m3_year) %>%
-#   mutate(airruse_m3_year = airruse*area,
-#          pirruse_m3_year = pirruse*area,
-#          bwa_m3_year = bwa_m_year*area,
-#          gwa_m3_year = gwa_m_year*area) %>%
-#   mutate(gws_rosa = pirruse_m3_year/ETc_m3_year, #Potential Irrigation Water Consumption to Potential Water Demands
-#          bws_rosa = airruse_m3_year/bwa_m3_year, #Irrigation Water Consumption to Blue Water Availability
-#          gws_liu = ETc_m3_year/gwa_m3_year,
-#          bws_liu = ETc_m3_year/(bwa_m3_year+gwa_m3_year)) %>%
-#   mutate(gws_rosa_bin = ifelse(gws_rosa > 0.1, 1, 0),
-#          bws_rosa_bin = ifelse(bws_rosa > 1, 1, 0),
-#          bws_liu_trunc = ifelse(bws_liu > 2, 2, bws_liu))
+ds_pixel_year = pixel_level_scarcity(ds1_transform, withdrawal_based = withdrawal_based)
 
-#Get yearly water metrics by pixel by year
-ds_pixel_year = 
-  ds1_transform %>%
-  merge(ref_raster_df, by = c('lon', 'lat'), all.x=T) %>%
-  merge(ds2_transform, by = c('lon', 'lat', "year", "month"), all.x=T) %>%
-  dplyr::select(lon, lat, area:REGION_WB, year, month, gwa_m_month, bwa_m_month, airrww, aindww, aliveww, adomww,
-                airruse, ainduse, aliveuse, adomuse, pirruse, ETc) %>%
-  mutate(across(airrww:pirruse, ~ .x*area, .names = "{.col}_m3"),
-         ETc_m3 = ETc,
-         gwa_m3 = gwa_m_month*area,
-         bwa_m3 = bwa_m_month*area) %>%
-  mutate(actual_water_withdrawal_m3 = airrww_m3+aindww_m3+aliveww_m3+adomww_m3,
-         actual_water_consumed_m3 = airruse_m3+ainduse_m3+aliveuse_m3+adomuse_m3) %>%
-  #Calculate scarcity at the monthly-scale
-  mutate(gws_pixel = pirruse_m3/ETc_m3, #Potential Irrigation Water Consumption to Potential Water Demands
-         bws_pixel = actual_water_consumed_m3/bwa_m3 #Irrigation Water Consumption to Blue Water Availability
-  ) %>%
-  mutate(gws_pixel_bin = ifelse(gws_pixel > 0.1, 1, 0), #Estimate the monthly stressed regions
-         bws_pixel_bin = ifelse(bws_pixel > 1, 1, 0)) %>%
-  group_by(lon, lat, WB_NAME, ISO_A3, year) %>% #Convert from monthly to yearly by taking the sum
-  summarise(across(airrww_m3:actual_water_consumed_m3, ~ sum(.x, na.rm = T), .names = "{.col}_year"),
-            across(gws_pixel:bws_pixel, ~median(.x, na.rm = T), .names = "{.col}_mean"),
-            across(gws_pixel_bin:bws_pixel_bin, ~ sum(.x, na.rm = T), .names = "{.col}_year")) %>%
-  #Calculate scarcity at the yearlu-scale
-  mutate(gws_pixel_year = pirruse_m3_year/ETc_m3_year, 
-         bws_pixel_year = actual_water_consumed_m3_year/bwa_m3_year) %>%
-  as.data.table() %>%
-  filter(!is.na(WB_NAME))
-
-#Get yearly water metrics by country by year
-ds_country_year = 
-  ds_pixel_year %>%
-  group_by(year, WB_NAME, ISO_A3) %>%
-  summarise(across(airrww_m3_year:actual_water_consumed_m3_year, ~ sum(.x, na.rm = T)),
-            across(gws_pixel_mean:bws_pixel_year, ~median(.x, na.rm = T), .names = "{.col}_median")) %>%
-  mutate(gws_year_country = pirruse_m3_year/ETc_m3_year, #Potential Irrigation Water Consumption to Potential Water Demands
-         bws_year_country = actual_water_consumed_m3_year/bwa_m3_year #Irrigation Water Consumption to Blue Water Availability
-  ) %>%
-  merge(WF_data, by = 'WB_NAME', all.x = T) %>%
-  as.data.table() %>%
-  dplyr::select(year, WB_NAME, ISO_A3, gws_pixel_mean_median:bws_year_country) %>%
-  dplyr::rename(gws_monthly_aggregate = gws_pixel_mean_median, bws_monthly_aggregate = bws_pixel_mean_median,
-                gws_monthly_binary = gws_pixel_bin_year_median, gws_monthly_binary = gws_pixel_bin_year_median,
-                gws_pixel_year = gws_pixel_year_median, bws_pixel_year = bws_pixel_year_median) %>%
-  arrange(WB_NAME, year)
+ds_country_year = country_level_scarcity(ds_pixel_year, withdrawal_based = withdrawal_based,
+                                         cropland_threshold = F)
 
 ###############
 #fwrite(ds_country_year, paste0(proj_dir, 'scarcity_outputs/country_level_metrics_1981_2005.csv'))
-fwrite(ds_country_year, paste0(proj_dir, 'scarcity_outputs/country_scarcity_metrics_1981_2005.csv'))
+#fwrite(ds_country_year, paste0(proj_dir, 'scarcity_outputs/country_scarcity_metrics_CONSUMPTION_1981_2005.csv'))
+#fwrite(ds_country_year, paste0(proj_dir, 'scarcity_outputs/country_scarcity_metrics_WITHDRAWAL_1981_2005.csv'))
+
+fwrite(ds_country_year, paste0(proj_dir, 'scarcity_outputs/country_scarcity_metrics_CONSUMPTION_allPixels_1981_2005.csv'))
+#fwrite(ds_country_year, paste0(proj_dir, 'scarcity_outputs/country_scarcity_metrics_WITHDRAWAL_allPixels_1981_2005.csv'))
+
+
+#test = fread(paste0(proj_dir, 'scarcity_outputs/country_level_metrics_1981_2005.csv'))
 ##############
 
 #Checks
-ds_all_sub = 
+ds_for_raster = 
   ds_pixel_year %>%
-  filter(year == 2005) 
+  filter(year == 2005) #%>% filter(cropland_5 == 1)
 
-ds_all_sub =
+ds_for_country =
   ds_country_year %>%
-  #filter(year == 2005) %>%
+  filter(year == 2005) %>%
   filter(WB_NAME == 'India') 
   # mutate(ratio_BW_WF = actual_water_consumed_m3_year/WF_Blue_Total_m3_year,
   #        ratio_GW_WF = gwa_m3_year/WF_Green_Total_m3_year) %>%
   #dplyr::select(WB_NAME, ratio_GW_WF)
 
-check_raster = rasterFromXYZ(ds_all_sub[,.(lon, lat, actual_water_withdrawal_m3_year)])
-plot(check_raster, main = 'Green Water Scarce Regions')  
+ds_all_sub =
+  ds_country_year %>%
+  filter(year > 2000) %>%
+  #filter(WB_NAME == 'India') 
+  group_by(WB_NAME, ISO_A3) %>%
+  summarise(across(gws_monthly_aggregate:bws_year_country, ~median(.x, na.rm=T))) %>%
+  as.data.table()
+# mutate(ratio_BW_WF = actual_water_consumed_m3_year/WF_Blue_Total_m3_year,
+#        ratio_GW_WF = gwa_m3_year/WF_Green_Total_m3_year) %>%
+#dplyr::select(WB_NAME, ratio_GW_WF)
+
+check_raster = rasterFromXYZ(ds_for_raster[,.(lon, lat, gws_pixel_year)])
+plot(check_raster, col = map.pal("viridis"), main = 'Green Water Scarcity',
+     zlim = c(0.1,1))  
 
 #Hoekstra vs PCR-GLOBWB
 plot(log(ds_all_sub$WF_Blue_Total_m3_year), log(ds_all_sub$actual_water_consumed_m3_year),
@@ -315,6 +269,18 @@ plot(log(ds_all_sub$WF_Green_Total_m3_year), log(ds_all_sub$gwa_m3_year),
      ylab = 'Actual Green ET (m3/yr; PCR-GLOBWB)', ylim = c(13,30),
      main = 'Green Water Consumption comparison (log-scale)')
 abline(coef = c(0,1))
+
+plot(log(ds_all_sub$gws_year_country), log(ds_all_sub$bws_year_country),
+     xlab = 'Log - Green Water Stress (option 4)',
+     ylab = 'Log - Blue Water Stress (option 4)',
+     main = 'BWS vs GWS (log-scale)')
+abline(coef = c(0,1))
+
+plot((ds_all_sub$gws_pixel_year), (ds_all_sub$bws_pixel_year),
+     xlab = 'Green Water Scarcity (option 3)',
+     ylab = 'Blue Water Scarcity (option 3)', ylim = c(0,0.3),
+     main = 'BWS vs GWS')
+#abline(coef = c(0,1))
 
 ##########################################################################################
 #Estimate Rosa/Liu metrics
---
+++ 4_scarcity_metrics/workflow_functions.R
@@ -0,0 +1,254 @@
+
+pre_process_PCR_GLOBWB = function(df, withdrawal_based){
+  #Reference for unit conversion: https://www.researchgate.net/post/How-to-convert-cmip5-monthly-precipitation-kg-m2-s1-into-mm-month
+  if(withdrawal_based == T) {
+    print("Assuming no environmental flow requirements")
+    env_factor = 0 #proportion of runoff left for environmental demands
+  } else {
+    env_factor = 0.4 #proportion of runoff left for environmental demands
+  }
+  
+  #Monthly
+  output = 
+    df %>%
+    dplyr::select(lon:cell_id, qs, qsb, adomuse:pirrww) %>%
+    #filter(year>2000) %>% #Subset to the recent 5 years
+    mutate(across(qs:pirrww, ~ .x*86400)) %>% #(kg m-2 s-1) to (kg m-2 day-1)
+    mutate(across(qs:pirrww, ~ .x*(days/1000))) %>% #(kg m-2 day-1) to (kg m-2 month-1) # kg m-2 == (1/1000) m 
+    mutate(bwa_m_month = (1-env_factor)*(qs + qsb),
+           gwa_m_month = airrusegreen + arainfusegreen) %>%
+    as.data.table()
+  
+  print("Data subsetted; converted units; estimated blue/green water availability")
+  
+  return(output)
+}
+
+pixel_level_scarcity = function(df, withdrawal_based){
+  print("0 - Kicking things off")
+  if(withdrawal_based == T) {
+    #Get yearly water metrics by pixel by year
+    output = 
+      df %>%
+      merge(ref_raster_df, by = c('lon', 'lat'), all.x=T) %>%
+      merge(ds2_transform, by = c('lon', 'lat', "year", "month"), all.x=T) %>%
+      dplyr::select(lon, lat, area:REGION_WB, year, month, gwa_m_month, bwa_m_month, airrww, aindww, aliveww, adomww,
+                    airruse, ainduse, aliveuse, adomuse, pirruse, ETc) 
+    ##########
+    print("1 - Datasets merged; variable subset taken")
+    ##########
+    output = 
+      output %>%
+      mutate(across(airrww:pirruse, ~ .x*area, .names = "{.col}_m3"),
+             ETc_m3 = ETc,
+             gwa_m3 = gwa_m_month*area,
+             bwa_m3 = bwa_m_month*area) %>%
+      mutate(actual_water_withdrawal_m3 = airrww_m3+aindww_m3+aliveww_m3+adomww_m3,
+             actual_water_consumed_m3 = airruse_m3+ainduse_m3+aliveuse_m3+adomuse_m3) 
+    ##########
+    print("2 - Values converted to volumes; withdrawal and consumption calculated")
+    ##########
+    output = 
+      output %>%
+      #Calculate scarcity at the monthly-scale
+      mutate(gws_pixel = (ETc_m3 - gwa_m3)/ETc_m3, #Potential Irrigation Water Requirements to Potential Water Demands
+             bws_pixel = actual_water_withdrawal_m3/bwa_m3 #Irrigation Water Consumption to Blue Water Availability
+      ) 
+    ##########
+    print("3 - Monthly scarcity estimated for each pixel")
+    ##########
+    output = 
+      output %>%
+      mutate(gws_pixel = ifelse(gws_pixel<0, 0, gws_pixel)) %>% #Incase the gwa exceeds ETc
+      mutate(gws_pixel_bin = ifelse(gws_pixel > 0.1, 1, 0), #Estimate the monthly stressed regions
+             bws_pixel_bin = ifelse(bws_pixel > 1, 1, 0)) 
+    ##########
+    print("4 - Scarcity values converted to binary for each month for each pixel")
+    ##########
+    output = 
+      output %>%
+      group_by(lon, lat, WB_NAME, ISO_A3, year) %>% #Convert from monthly to yearly by taking the sum
+      summarise(across(airrww_m3:actual_water_consumed_m3, ~ sum(.x, na.rm = T), .names = "{.col}_year"),
+                across(gws_pixel:bws_pixel, ~median(.x, na.rm = T), .names = "{.col}_mean"),
+                across(gws_pixel_bin:bws_pixel_bin, ~ sum(.x, na.rm = T), .names = "{.col}_year")) 
+    ##########
+    print("5 - Monthly values converted to yearly")
+    ##########
+    output = 
+      output %>%
+      #Calculate scarcity at the yearlu-scale
+      mutate(gws_pixel_year = (ETc_m3_year - gwa_m3_year)/ETc_m3_year, 
+             bws_pixel_year = actual_water_withdrawal_m3_year/bwa_m3_year) %>%
+      mutate(gws_pixel_year = ifelse(gws_pixel_year<0, 0, gws_pixel_year)) %>% #Incase the gwa exceeds ETc
+      as.data.table() %>%
+      filter(!is.na(WB_NAME)) %>%
+      merge(df_crop, by = c('lon', 'lat'), all.x=T) #Add a cropland layer to remove low ag regions
+    
+    ##########
+    print("6 - Yearly scarcity calculated; crop layer merged")
+    ##########
+    
+  } else {
+    
+    #Get yearly water metrics by pixel by year
+    output = 
+      df %>%
+      merge(ref_raster_df, by = c('lon', 'lat'), all.x=T) %>%
+      merge(ds2_transform, by = c('lon', 'lat', "year", "month"), all.x=T) %>%
+      dplyr::select(lon, lat, area:REGION_WB, year, month, gwa_m_month, bwa_m_month, airrww, aindww, aliveww, adomww,
+                    airruse, ainduse, aliveuse, adomuse, pirruse, ETc) 
+    ##########
+    print("1 - Datasets merged; variable subset taken")
+    ##########
+    output = 
+      output %>%
+      mutate(across(airrww:pirruse, ~ .x*area, .names = "{.col}_m3"),
+             ETc_m3 = ETc,
+             gwa_m3 = gwa_m_month*area,
+             bwa_m3 = bwa_m_month*area) %>%
+      mutate(actual_water_withdrawal_m3 = airrww_m3+aindww_m3+aliveww_m3+adomww_m3,
+             actual_water_consumed_m3 = airruse_m3+ainduse_m3+aliveuse_m3+adomuse_m3) 
+    ##########
+    print("2 - Values converted to volumes; withdrawal and consumption calculated")
+    ##########
+    output = 
+      output %>%
+      #Calculate scarcity at the monthly-scale
+      mutate(gws_pixel = (ETc_m3 - gwa_m3)/ETc_m3, #Potential Irrigation Water Requirements to Potential Water Demands
+             bws_pixel = actual_water_consumed_m3/bwa_m3 #Irrigation Water Consumption to Blue Water Availability
+      ) 
+    ##########
+    print("3 - Monthly scarcity estimated for each pixel")
+    ##########
+    output = 
+      output %>%
+      mutate(gws_pixel = ifelse(gws_pixel<0, 0, gws_pixel)) %>% #Incase the gwa exceeds ETc
+      mutate(gws_pixel_bin = ifelse(gws_pixel > 0.1, 1, 0), #Estimate the monthly stressed regions
+             bws_pixel_bin = ifelse(bws_pixel > 1, 1, 0)) 
+    ##########
+    print("4 - Scarcity values converted to binary for each month for each pixel")
+    ##########
+    output = 
+      output %>%
+      group_by(lon, lat, WB_NAME, ISO_A3, year) %>% #Convert from monthly to yearly by taking the sum
+      summarise(across(airrww_m3:actual_water_consumed_m3, ~ sum(.x, na.rm = T), .names = "{.col}_year"),
+                across(gws_pixel:bws_pixel, ~median(.x, na.rm = T), .names = "{.col}_mean"),
+                across(gws_pixel_bin:bws_pixel_bin, ~ sum(.x, na.rm = T), .names = "{.col}_year")) 
+    ##########
+    print("5 - Monthly values converted to yearly")
+    ##########
+    output = 
+      output %>%
+      #Calculate scarcity at the yearlu-scale
+      mutate(gws_pixel_year = (ETc_m3_year - gwa_m3_year)/ETc_m3_year, 
+             bws_pixel_year = actual_water_consumed_m3_year/bwa_m3_year) %>%
+      mutate(gws_pixel_year = ifelse(gws_pixel_year<0, 0, gws_pixel_year)) %>% #Incase the gwa exceeds ETc
+      as.data.table() %>%
+      filter(!is.na(WB_NAME)) %>%
+      merge(df_crop, by = c('lon', 'lat'), all.x=T) #Add a cropland layer to remove low ag regions
+    
+    ##########
+    print("6 - Yearly scarcity calculated; crop layer merged")
+    ##########
+    
+  }
+  
+  return(output)
+}
+
+country_level_scarcity = function(df, withdrawal_based, cropland_threshold = F){
+  print("0 - Kicking things off")
+  
+  if(cropland_threshold == T){
+    print("1 - Applying cropland threshold")
+    #Get yearly water metrics by country by year
+    output = 
+      df %>%
+      #Essentially remove the non-croplands; this prevents regions with limited cropped area
+      #from skewing the aggregated metrics
+      mutate(gws_pixel_mean = ifelse(cropland_5 == 1, gws_pixel_mean, NA),
+             gws_pixel_year = ifelse(cropland_5 == 1, gws_pixel_year, NA))
+  } else {
+    print("1 - NOT Applying cropland threshold")
+    #Get yearly water metrics by country by year
+    output = 
+      df
+  }
+  
+  if(withdrawal_based == T) {
+    
+    output = 
+      output %>%
+      group_by(year, WB_NAME, ISO_A3) %>%
+      summarise(across(airrww_m3_year:actual_water_consumed_m3_year, ~ sum(.x, na.rm = T)),
+                across(gws_pixel_mean:bws_pixel_year, ~median(.x, na.rm = T), .names = "{.col}_median")) 
+    
+    ##########
+    print("2 - Pixel values aggregated to country-level")
+    ##########
+    
+    output = 
+      output %>%
+      mutate(gws_year_country = (ETc_m3_year - gwa_m3_year)/ETc_m3_year, #Potential Irrigation Water Consumption to Potential Water Demands
+             bws_year_country = actual_water_withdrawal_m3_year/bwa_m3_year #Irrigation Water Consumption to Blue Water Availability
+      ) %>%
+      mutate(gws_year_country = ifelse(gws_year_country<0, 0, gws_year_country)) 
+    
+    ##########
+    print("3 - Country-level scarcity calculated")
+    ##########
+    
+    output = 
+      output %>% #Incase the gwa exceeds ETc
+      merge(WF_data, by = 'WB_NAME', all.x = T) %>%
+      as.data.table() %>%
+      dplyr::select(year, WB_NAME, ISO_A3, gws_pixel_mean_median:bws_year_country) %>%
+      dplyr::rename(gws_monthly_aggregate = gws_pixel_mean_median, bws_monthly_aggregate = bws_pixel_mean_median,
+                    gws_monthly_binary = gws_pixel_bin_year_median, gws_monthly_binary = gws_pixel_bin_year_median,
+                    gws_pixel_year = gws_pixel_year_median, bws_pixel_year = bws_pixel_year_median) %>%
+      arrange(WB_NAME, year)
+    
+    ##########
+    print("4 - Water Footprint data merged; variables subsetted")
+    ##########
+  } else {
+    
+    output = 
+      output %>%
+      group_by(year, WB_NAME, ISO_A3) %>%
+      summarise(across(airrww_m3_year:actual_water_consumed_m3_year, ~ sum(.x, na.rm = T)),
+                across(gws_pixel_mean:bws_pixel_year, ~median(.x, na.rm = T), .names = "{.col}_median")) 
+    
+    ##########
+    print("2 - Pixel values aggregated to country-level")
+    ##########
+    
+    output = 
+      output %>%
+      mutate(gws_year_country = (ETc_m3_year - gwa_m3_year)/ETc_m3_year, #Potential Irrigation Water Consumption to Potential Water Demands
+             bws_year_country = actual_water_consumed_m3_year/bwa_m3_year #Irrigation Water Consumption to Blue Water Availability
+      ) %>%
+      mutate(gws_year_country = ifelse(gws_year_country<0, 0, gws_year_country)) 
+    
+    ##########
+    print("3 - Country-level scarcity calculated")
+    ##########
+    
+    output = 
+      output %>% #Incase the gwa exceeds ETc
+      merge(WF_data, by = 'WB_NAME', all.x = T) %>%
+      as.data.table() %>%
+      dplyr::select(year, WB_NAME, ISO_A3, gws_pixel_mean_median:bws_year_country) %>%
+      dplyr::rename(gws_monthly_aggregate = gws_pixel_mean_median, bws_monthly_aggregate = bws_pixel_mean_median,
+                    gws_monthly_binary = gws_pixel_bin_year_median, gws_monthly_binary = gws_pixel_bin_year_median,
+                    gws_pixel_year = gws_pixel_year_median, bws_pixel_year = bws_pixel_year_median) %>%
+      arrange(WB_NAME, year)
+    
+    ##########
+    print("4 - Water Footprint data merged; variables subsetted")
+    ##########
+  }
+  
+  return(output)
+}
+
